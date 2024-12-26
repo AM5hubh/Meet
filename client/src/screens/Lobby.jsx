fc7d9bcb3c471704ebf891aa7a6b40d419945acb
AM5hubh <ssahamate2020@gmail.com>
2024-12-26 23:11:32 +0530

    Room added

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -1,17 +1,34 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSocket } from "../context/SocketProvider";
 const Lobby = () => {
   const [email, setEmail] = useState("");
   const [room, setRoom] = useState("");
+  const navigate = useNavigate();
+  const socket = useSocket();
+  console.log(socket);
 
-  const socket = useSocket()
-  console.log(socket)
-  
-  const handleSubmit = useCallback((e) => {
-    e.preventDefault();
-    socket.emit('room:join', { email, room });
-    console.log({ email, room });
-  }, [email, room, socket]);
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      socket.emit("room:join", { email, room });
+      console.log({ email, room });
+    },
+    [email, room, socket]
+  );
+
+  const handlejoinroom = useCallback((data) => {
+    const { email, room } = data;
+    navigate(`/room/${room}`);
+  }, [navigate]);
+
+  useEffect(() => {
+    socket.on("room:join", handlejoinroom);
+    return () => {
+      socket.off("room:join", handlejoinroom);
+    };
+  }, [socket, handlejoinroom]);``
+
   return (
     <div>
       <div>Lobby</div>
--- server/index.js
+++ server/index.js
@@ -3,9 +3,16 @@ import { Server } from "socket.io";
 const io = new Server(8000,{
     cors: true,
 })
+
+const emailtoSocketIdMap = new Map()
+const socketIdtoEmailMap = new Map()
+
 io.on('connection', (socket) => {
     console.log('socket connected',socket.id);
     socket.on('room:join', (data) => {
-        console.log(data);
+        const {email,room} = data;
+        emailtoSocketIdMap.set(email,socket.id);
+        socketIdtoEmailMap.set(socket.id,email);
+        io.to(socket.id).emit("room:join", data)
     });
 });
